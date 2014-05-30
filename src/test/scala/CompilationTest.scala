726457ed642c650c9c62344d5b0ea5c540c21998
Dmytro Starosud <dmytro.starosud@strikead.com>
2014-05-30 16:31:30 +0300

    non-solution

--- src/test/scala/CompilationTest.scala
+++ src/test/scala/CompilationTest.scala
@@ -12,14 +12,10 @@ class CompilationTest {
 
   case class Cons[+H, +T <: Vector[H]](h: H, t: T) extends Vector[H]
 
-  type Vec[N <: TNat, T] = Fix#Fix[TNat, Vector[T], ({
+  type Vec[N <: TNat, T] = FixImpl#Fix[TNat, Vector[T], ({
     type R[F[_ <: TNat] <: Vector[T], N <: TNat] =
     N#If[Vector[T], VNil.type, ({type R[N <: TNat] = Cons[T, F[N]]})#R]
   })#R, N]
 
-  implicitly[VNil.type =:= Vec[Zero, Any]]
-
-  //    implicitly[VNil.type =:= Vec[Zero, Nothing]]
-
-  //    implicitly[VNil.type =:= Vec[Succ[Zero], Nothing]]
+  //implicitly[VNil.type =:= Vec[Zero, Nothing]]
 }
